974520c0e48301f1d398d4a202295b79806c0c19
djo011 <djo011@uib.no>
2025-10-04 16:37:30 +0200

    add basic functionality for retrieving data from openLib api

---
+++ src/my-athenaeum/openlib.py
@@ -0,0 +1,58 @@
+import requests
+
+
+class OpenLib:
+    """Connection to the OpenLib API/Web page to retrieve information about books."""
+
+    url = "https://openlibrary.org"
+    headers = {
+        "User-Agent": "MyAthenaeum/1.0 (danielhamj97@gmail.com)",
+        # "Accept": "application.json"
+    }
+
+    def get_author(self, author_key: str) -> dict | None:
+        author_url = self.url + author_key + ".json"
+        response = requests.get(url=author_url, headers=self.headers)
+
+        if response.status_code != 200:
+            print(
+                "Failed to retrieve auhtor! Response was %s with reason %s",
+                response.status_code,
+                response.reason,
+            )
+            return None
+
+        return response.content.decode(encoding="utf-8")
+
+    def get_work(self, work_key: str) -> dict | None:
+        work_url = self.url + work_key + ".json"
+        response = requests.get(url=work_url, headers=self.headers)
+
+        if response.status_code != 200:
+            print(
+                "Failed to retrieve auhtor! Response was %s with reason %s",
+                response.status_code,
+                response.reason,
+            )
+            return None
+
+        return response.content.decode(encoding="utf-8")
+
+    def get_book(self, isbn: str) -> dict | None:
+        book_url = self.url + "/isbn/" + isbn + ".json"
+        response = requests.get(url=book_url, headers=self.headers)
+
+        if response.status_code != 200:
+            print(
+                "Failed to retrieve auhtor! Response was %s with reason %s",
+                response.status_code,
+                response.reason,
+            )
+            return None
+
+        return response.content.decode(encoding="utf-8")
+
+
+# OpenLib().get_author(author_key="/authors/OL34221A")
+# print(OpenLib().get_work(work_key="/work/OL46347W"))
+print(OpenLib().get_book(isbn="9780008117535"))
