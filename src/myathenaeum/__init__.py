021ed375a03c5d249cfe45eebd1cddb25a116fe7
djo011 <djo011@uib.no>
2025-10-19 17:29:01 +0200

    ruff format changes

--- src/myathenaeum/__init__.py
+++ src/myathenaeum/__init__.py
@@ -1,10 +1,11 @@
 """Module for my Athenaeum"""
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///athenaeum.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Avoids a warning
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///athenaeum.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False  # Avoids a warning
 db = SQLAlchemy(app)
 
 from myathenaeum import routes
